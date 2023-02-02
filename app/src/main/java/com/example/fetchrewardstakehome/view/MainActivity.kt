0540f6062cd4ce0a49ac6f14552b1ebb2ce556ba
Apoorva Aditya <apoorva9@illinois.edu>
2023-02-02 13:48:36 -0600

    Moved MainActivity to view folder

--- app/src/main/java/com/example/fetchrewardstakehome/view/MainActivity.kt
+++ app/src/main/java/com/example/fetchrewardstakehome/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.fetchrewardstakehome
+package com.example.fetchrewardstakehome.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fetchrewardstakehome.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
