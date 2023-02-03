2cd5fad32ee0dcc9d5252dde07b13090cdc9b2f9
Apoorva Aditya <apoorva9@illinois.edu>
2023-02-03 15:43:36 -0600

    Added comments

--- app/src/main/java/com/example/fetchrewardstakehome/common/AsyncStatus.kt
+++ app/src/main/java/com/example/fetchrewardstakehome/common/AsyncStatus.kt
@@ -1,5 +1,6 @@
-package com.example.fetchrewardstakehome.utilities
+package com.example.fetchrewardstakehome.common
 
+// Sealed Class to represent the status of an asynchronous operation
 sealed class AsyncStatus<out T> {
     data class Success<out R>(val value: R) : AsyncStatus<R>()
     data class Failure(val message: String): AsyncStatus<Nothing>()
