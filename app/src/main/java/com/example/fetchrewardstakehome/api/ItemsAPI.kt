e20c6f97b2dd66f3cf7ddff8bbed5a4abf25065a
Apoorva Aditya <apoorva9@illinois.edu>
2023-02-02 23:57:48 -0600

    Created Retrofit API interface and setup

---
+++ app/src/main/java/com/example/fetchrewardstakehome/api/ItemsAPI.kt
@@ -0,0 +1,24 @@
+package com.example.fetchrewardstakehome.api
+
+import com.example.fetchrewardstakehome.model.Item
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface ItemsAPI {
+    @GET("/hiring.json")
+    suspend fun getItems() : Response<List<Item>>
+
+    companion object {
+        private const val BASE_URL = "https://fetch-hiring.s3.amazonaws.com"
+
+        fun create() : ItemsAPI {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ItemsAPI::class.java)
+        }
+    }
+}
